751e2f5ed16bebad0b05842f4f6dfe151bbe0817
maamokun/MikanDev <sachadeco214@gmail.com>
2025-05-27 13:06:02 +0900

    feat: proper global configuration validation

--- src/index.ts
+++ src/index.ts
@@ -25,10 +25,20 @@ export function setup(config: Config) {
 			"Invalid configuration. 'redirectUri' and 'jwtSecret' are required.",
 		);
 	}
+	if (!config.clientId || !config.clientSecret) {
+		if (!process.env.AUTH_DISCORD_ID || !process.env.AUTH_DISCORD_SECRET) {
+			throw new Error(
+				"You must provide 'clientId' and 'clientSecret' in the config or set them as environment variables AUTH_DISCORD_ID and AUTH_DISCORD_SECRET.",
+			);
+		}
+	}
+	if (globalConfig) {
+		console.warn("Global config is already set. Overwriting existing config.");
+	}
 	globalConfig = {
-		clientId: config.clientId || process.env.AUTH_DISCORD_ID || "",
+		clientId: config.clientId || process.env.AUTH_DISCORD_ID,
 		clientSecret:
-			config.clientSecret || process.env.AUTH_DISCORD_SECRET || "",
+			config.clientSecret || process.env.AUTH_DISCORD_SECRET,
 		scopes: config.scopes || ["identify", "email"],
 		redirectUri: config.redirectUri,
 		jwtSecret: config.jwtSecret,
